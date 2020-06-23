a8da39c3ac04139c608835d8e033945d5bf9c1fc
lrzeszotarski <lukas.rzeszotarski@outlook.com>
2020-06-23 13:53:08 +0200

    initial commit

---
+++ src/main/java/io/github/lrzeszotarski/accountmanager/AccountmanagerApplication.java
@@ -0,0 +1,13 @@
+package io.github.lrzeszotarski.accountmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountmanagerApplication.class, args);
+    }
+
+}
