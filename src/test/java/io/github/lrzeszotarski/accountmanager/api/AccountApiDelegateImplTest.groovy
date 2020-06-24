55563eeb4a56a178f8ce980d0a3f5e58ae1ddf93
lrzeszotarski <lukas.rzeszotarski@outlook.com>
2020-06-24 19:29:42 +0200

    EVA-0 api draft

---
+++ src/test/java/io/github/lrzeszotarski/accountmanager/api/AccountApiDelegateImplTest.groovy
@@ -0,0 +1,30 @@
+package io.github.lrzeszotarski.accountmanager.api
+
+class AccountApiDelegateImplTest extends spock.lang.Specification {
+    def "test createAccount"() {
+        given:
+
+        when:
+        // TODO implement stimulus
+        then:
+        // TODO implement assertions
+    }
+
+    def "test getAccounts"() {
+        given:
+
+        when:
+        // TODO implement stimulus
+        then:
+        // TODO implement assertions
+    }
+
+    def "test updateAccount"() {
+        given:
+
+        when:
+        // TODO implement stimulus
+        then:
+        // TODO implement assertions
+    }
+}
