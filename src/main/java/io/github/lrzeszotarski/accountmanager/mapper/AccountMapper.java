32900028e21f508c5d6368b19a37f9a3aaa72692
lrzeszotarski <lukas.rzeszotarski@outlook.com>
2020-06-25 15:34:45 +0200

    EVA-1 Create Account

--- src/main/java/io/github/lrzeszotarski/accountmanager/mapper/AccountMapper.java
+++ src/main/java/io/github/lrzeszotarski/accountmanager/mapper/AccountMapper.java
@@ -3,7 +3,7 @@ package io.github.lrzeszotarski.accountmanager.mapper;
 import io.github.lrzeszotarski.accountmanager.api.model.Account;
 import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface AccountMapper {
     Account toDto(io.github.lrzeszotarski.accountmanager.domain.entity.Account account);
 
