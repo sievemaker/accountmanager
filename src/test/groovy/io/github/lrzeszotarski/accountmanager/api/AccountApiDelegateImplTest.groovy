90710b273bdfb45f7e53c47e2e6d3d6e5a168182
lrzeszotarski <lukas.rzeszotarski@outlook.com>
2020-06-26 20:51:04 +0200

    EVA-6 Event Statistics

--- src/test/groovy/io/github/lrzeszotarski/accountmanager/api/AccountApiDelegateImplTest.groovy
+++ src/test/groovy/io/github/lrzeszotarski/accountmanager/api/AccountApiDelegateImplTest.groovy
@@ -2,7 +2,6 @@ package io.github.lrzeszotarski.accountmanager.api
 
 import io.github.lrzeszotarski.accountmanager.api.model.Account
 import io.github.lrzeszotarski.accountmanager.api.model.Event
-import io.github.lrzeszotarski.accountmanager.api.model.EventStatistics
 import io.github.lrzeszotarski.accountmanager.domain.service.AccountService
 import io.github.lrzeszotarski.accountmanager.domain.service.EventStatisticsService
 import io.github.lrzeszotarski.accountmanager.mapper.AccountMapper
