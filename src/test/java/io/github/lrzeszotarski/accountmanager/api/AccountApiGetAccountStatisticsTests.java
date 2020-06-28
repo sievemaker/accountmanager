54120d92fcbb99b77fcb40a2483410d884041f42
lrzeszotarski <lukas.rzeszotarski@outlook.com>
2020-06-28 22:02:04 +0200

    EVA-8 DAT

--- src/test/java/io/github/lrzeszotarski/accountmanager/api/AccountApiGetAccountStatisticsTests.java
+++ src/test/java/io/github/lrzeszotarski/accountmanager/api/AccountApiGetAccountStatisticsTests.java
@@ -5,6 +5,7 @@ import io.github.lrzeszotarski.accountmanager.api.model.Event;
 import io.github.lrzeszotarski.accountmanager.api.model.EventStatistics;
 import io.github.lrzeszotarski.accountmanager.domain.repository.AccountRepository;
 import io.github.lrzeszotarski.accountmanager.domain.repository.EventRepository;
+import io.github.lrzeszotarski.accountmanager.domain.service.EventCleanupService;
 import io.github.lrzeszotarski.accountmanager.scheduler.ScheduledTasks;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +20,6 @@ import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 
 @SpringBootTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
@@ -29,10 +29,10 @@ public class AccountApiGetAccountStatisticsTests {
     private AccountApiDelegate accountApiDelegate;
 
     @Autowired
-    private AccountRepository accountRepository;
+    private EventRepository eventRepository;
 
     @Autowired
-    private EventRepository eventRepository;
+    private EventCleanupService eventCleanupService;
 
     @MockBean
     private ScheduledTasks scheduledTasks;
@@ -69,6 +69,53 @@ public class AccountApiGetAccountStatisticsTests {
         accountApiDelegate.createEvent(createdAccount.getAccountId().toString(), event3);
         accountApiDelegate.createEvent(createdAccount.getAccountId().toString(), event4);
 
+        getStatisticsForGivenAccountAndDay(createdAccount, firstDate);
+    }
+
+    @Test
+    public void testGetAccountStatisticsAfterCleanupOutdatedEvents() {
+        final Account account = new Account();
+        account.setName("Sample Name");
+
+        final ResponseEntity<Account> createAccountResponseEntity = accountApiDelegate.createAccount(account);
+        final Account createdAccount = createAccountResponseEntity.getBody();
+
+        final OffsetDateTime firstDate = OffsetDateTime.now().minusDays(60);
+        final OffsetDateTime secondDate = OffsetDateTime.now().minusDays(61);
+
+        final Event event1 = new Event();
+        event1.setHappenedAt(firstDate);
+        event1.setType("Some Type");
+
+        final Event event2 = new Event();
+        event2. setHappenedAt(firstDate);
+        event2.setType("Some Type");
+
+        final Event event3 = new Event();
+        event3.setHappenedAt(secondDate);
+        event3.setType("Some Type");
+
+        final Event event4 = new Event();
+        event4.setHappenedAt(firstDate);
+        event4.setType("Some Other Type");
+
+        accountApiDelegate.createEvent(createdAccount.getAccountId().toString(), event1);
+        accountApiDelegate.createEvent(createdAccount.getAccountId().toString(), event2);
+        accountApiDelegate.createEvent(createdAccount.getAccountId().toString(), event3);
+        accountApiDelegate.createEvent(createdAccount.getAccountId().toString(), event4);
+
+        getStatisticsForGivenAccountAndDay(createdAccount, firstDate);
+
+        assertEquals(4, eventRepository.findAll().size());
+
+        eventCleanupService.cleanupEvents();
+
+        getStatisticsForGivenAccountAndDay(createdAccount, firstDate);
+
+        assertEquals(0, eventRepository.findAll().size());
+    }
+
+    private void getStatisticsForGivenAccountAndDay(Account createdAccount, OffsetDateTime firstDate) {
         final ResponseEntity<List<EventStatistics>> accountStatisticsResponseEntity =
                 accountApiDelegate.getAccountStatistics(
                         createdAccount.getAccountId().toString(),
