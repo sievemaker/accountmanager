5592f7f138dbd9044e7cf5656a437d5319997904
lrzeszotarski <lukas.rzeszotarski@outlook.com>
2020-06-28 18:40:46 +0200

    EVA-8 DAT

--- src/test/java/io/github/lrzeszotarski/accountmanager/api/AccountApiCreateEventTests.java
+++ src/test/java/io/github/lrzeszotarski/accountmanager/api/AccountApiCreateEventTests.java
@@ -16,7 +16,6 @@ import java.time.OffsetDateTime;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
 
 @SpringBootTest
 public class AccountApiCreateEventTests {
@@ -40,6 +39,7 @@ public class AccountApiCreateEventTests {
         final OffsetDateTime now = OffsetDateTime.now();
         final ResponseEntity<Account> createAccountResponse = accountApiDelegate.createAccount(account);
         final Account createdAccount = createAccountResponse.getBody();
+        io.github.lrzeszotarski.accountmanager.domain.entity.Account savedAccount = accountRepository.findByAccountId(createdAccount.getAccountId());
         final Event firstEvent = new Event();
         firstEvent.setHappenedAt(now);
         firstEvent.setType("Some Type");
@@ -49,12 +49,28 @@ public class AccountApiCreateEventTests {
         final ResponseEntity<Event> createdFirstEventResponse = accountApiDelegate.createEvent(createdAccount.getAccountId().toString(), firstEvent);
         assertEquals(HttpStatus.OK, createdFirstEventResponse.getStatusCode());
         final Event createdFirstEvent = createdFirstEventResponse.getBody();
+        assertEquals(now, createdFirstEvent.getHappenedAt());
+        assertEquals("Some Type", createdFirstEvent.getType());
 
         final ResponseEntity<Event> createdSecondEventResponse = accountApiDelegate.createEvent(createdAccount.getAccountId().toString(), secondEvent);
         assertEquals(HttpStatus.OK, createdSecondEventResponse.getStatusCode());
+        final Event createdSecondEvent = createdSecondEventResponse.getBody();
+        assertEquals(now, createdSecondEvent.getHappenedAt());
+        assertEquals("Some Type", createdSecondEvent.getType());
 
         final List<io.github.lrzeszotarski.accountmanager.domain.entity.Event> eventList = eventRepository.findAll();
         assertEquals(2, eventList.size());
-        io.github.lrzeszotarski.accountmanager.domain.entity.Event firstSavedEvent = eventList.get(0);
+        final io.github.lrzeszotarski.accountmanager.domain.entity.Event firstSavedEvent = eventList.get(0);
+        final io.github.lrzeszotarski.accountmanager.domain.entity.Event secondSavedEvent = eventList.get(1);
+
+        assertEquals(now, firstSavedEvent.getHappenedAt());
+        assertEquals(savedAccount.getId(), firstSavedEvent.getAccount().getId());
+        assertEquals("Some Type", firstSavedEvent.getType());
+        assertEquals(createdFirstEvent.getEventId(), firstSavedEvent.getEventId());
+
+        assertEquals(now, secondSavedEvent.getHappenedAt());
+        assertEquals(savedAccount.getId(), secondSavedEvent.getAccount().getId());
+        assertEquals("Some Type", secondSavedEvent.getType());
+        assertEquals(createdSecondEvent.getEventId(), secondSavedEvent.getEventId());
     }
 }
