5bc50ca1b68cbc454a72a1d26b927905c92919f2
lrzeszotarski <lukas.rzeszotarski@outlook.com>
2020-06-28 22:28:04 +0200

    EVA-8 DAT

--- src/test/java/io/github/lrzeszotarski/accountmanager/AccountmanagerApplicationTests.java
+++ src/test/java/io/github/lrzeszotarski/accountmanager/AccountmanagerApplicationTests.java
@@ -1,10 +1,15 @@
 package io.github.lrzeszotarski.accountmanager;
 
+import io.github.lrzeszotarski.accountmanager.scheduler.ScheduledTasks;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class AccountmanagerApplicationTests {
+
+    @MockBean
+    private ScheduledTasks scheduledTasks;
 
     @Test
     void contextLoads() {
